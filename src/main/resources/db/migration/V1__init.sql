4d89fe85f54dd111fb36306e29757f8203502dc6
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-05 22:02:49 +0400

    feat: add Flyway migration init SQL script `V1__init.sql`

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users
+(
+    id       BIGINT       NOT NULL GENERATED ALWAYS AS IDENTITY,
+    username VARCHAR(35)  NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
