f7a19fc0bb65ade4ebb3c76d792ee5b13a549f1f
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-05 22:01:39 +0400

    feat: add `User` entity

---
+++ src/main/java/com/asalavei/cloudfilestorage/entity/User.java
@@ -0,0 +1,36 @@
+package com.asalavei.cloudfilestorage.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username", length = 35, nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
