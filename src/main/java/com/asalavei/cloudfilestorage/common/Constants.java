83bd8a97e38bd89582b633a4b221d671c327d91f
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-06 16:16:10 +0400

    feat: add `Constants`

---
+++ src/main/java/com/asalavei/cloudfilestorage/common/Constants.java
@@ -0,0 +1,22 @@
+package com.asalavei.cloudfilestorage.common;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Constants {
+
+    public static final String USER_ATTRIBUTE = "user";
+    public static final String ERROR_MESSAGE_ATTRIBUTE = "errorMessage";
+
+    public static final String REDIRECT_HOME = "redirect:/";
+    public static final String REDIRECT_SIGNIN = "redirect:/auth/signin";
+
+    public static final String HOME_URL = "/";
+    public static final String SIGNIN_URL = "/auth/signin";
+    public static final String SIGNUP_URL = "/auth/signup";
+
+    public static final String SIGNIN_VIEW = "auth/signin";
+    public static final String SIGNUP_VIEW = "auth/signup";
+    public static final String HOME_VIEW = "home";
+}
