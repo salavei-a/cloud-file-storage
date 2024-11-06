1fbffa9320c0f591c39c838e19ca5646f24e93fd
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-06 16:14:46 +0400

    feat: implement `UserDetails` with `UserPrincipal`

---
+++ src/main/java/com/asalavei/cloudfilestorage/security/UserPrincipal.java
@@ -0,0 +1,52 @@
+package com.asalavei.cloudfilestorage.security;
+
+import com.asalavei.cloudfilestorage.entity.User;
+import lombok.Builder;
+import lombok.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Builder
+@Value
+public class UserPrincipal implements UserDetails {
+
+    User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
