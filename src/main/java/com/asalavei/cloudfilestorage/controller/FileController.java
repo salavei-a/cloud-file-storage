fd4305f3756faf2e94414e60a55e09c1b1593f60
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-07 21:56:39 +0400

    feat: implement upload folder in `FileController`

--- src/main/java/com/asalavei/cloudfilestorage/controller/FileController.java
+++ src/main/java/com/asalavei/cloudfilestorage/controller/FileController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 import static com.asalavei.cloudfilestorage.common.Constants.REDIRECT_HOME;
 
 @Controller
@@ -20,8 +22,14 @@ public class FileController {
     private final StorageService storageService;
 
     @PostMapping("/upload")
-    public String uploadFile(@RequestParam("file")MultipartFile file, @AuthenticationPrincipal UserPrincipal userPrincipal) {
-        storageService.uploadFile(userPrincipal.getId(),file);
+    public String uploadFile(@RequestParam("file") MultipartFile file, @AuthenticationPrincipal UserPrincipal userPrincipal) {
+        storageService.uploadFile(userPrincipal.getId(), file);
+        return REDIRECT_HOME;
+    }
+
+    @PostMapping("/upload-folder")
+    public String uploadFolder(@RequestParam("files") List<MultipartFile> files, @AuthenticationPrincipal UserPrincipal userPrincipal) {
+        files.forEach(file -> storageService.uploadFile(userPrincipal.getId(), file));
         return REDIRECT_HOME;
     }
 }
