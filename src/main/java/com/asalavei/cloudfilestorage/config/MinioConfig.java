b0b48851229d82fd9c98d2fbe02e5ae3b0d88d2f
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-07 20:59:53 +0400

    feat: add `Minio` configuration

---
+++ src/main/java/com/asalavei/cloudfilestorage/config/MinioConfig.java
@@ -0,0 +1,47 @@
+package com.asalavei.cloudfilestorage.config;
+
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioConfig {
+
+    @Value("${minio.url}")
+    private String url;
+
+    @Value("${minio.access.name}")
+    private String accessKey;
+
+    @Value("${minio.access.secret}")
+    private String secretKey;
+
+    @Value("${minio.bucket.name}")
+    private String bucketName;
+
+    @Bean
+    public MinioClient minioClient() {
+        MinioClient minioClient = MinioClient.builder()
+                .endpoint(url)
+                .credentials(accessKey, secretKey)
+                .build();
+
+        initBucket(minioClient);
+
+        return minioClient;
+    }
+
+    private void initBucket(MinioClient minioClient) {
+        try {
+            boolean bucketExists = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+            if (!bucketExists) {
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Could not initialize bucket: " + bucketName, e); // TODO: custom exception
+        }
+    }
+}
