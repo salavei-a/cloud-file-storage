13b25a65c7f712c688491c71541fcc580470a544
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-07 21:09:42 +0400

    feat: implement create folder method in `HomeController`

--- src/main/java/com/asalavei/cloudfilestorage/controller/HomeController.java
+++ src/main/java/com/asalavei/cloudfilestorage/controller/HomeController.java
@@ -1,19 +1,33 @@
 package com.asalavei.cloudfilestorage.controller;
 
+import com.asalavei.cloudfilestorage.security.UserPrincipal;
+import com.asalavei.cloudfilestorage.service.StorageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import static com.asalavei.cloudfilestorage.common.Constants.HOME_VIEW;
+import static com.asalavei.cloudfilestorage.common.Constants.REDIRECT_HOME;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/")
 public class HomeController {
 
+    private final StorageService storageService;
+
     @GetMapping
     public String homePage() {
         return HOME_VIEW;
+    }
+
+    @PostMapping("/create-folder")
+    public String createFolder(@RequestParam("folderName") String folderName, @AuthenticationPrincipal UserPrincipal userPrincipal) {
+        storageService.createFolder(userPrincipal.getId(), folderName);
+        return REDIRECT_HOME;
     }
 }
