f779c8b277ba4e3373657a6a639b30491af1d589
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-07 21:09:01 +0400

    feat: implement upload file method in `FileController`

---
+++ src/main/java/com/asalavei/cloudfilestorage/controller/FileController.java
@@ -0,0 +1,27 @@
+package com.asalavei.cloudfilestorage.controller;
+
+import com.asalavei.cloudfilestorage.security.UserPrincipal;
+import com.asalavei.cloudfilestorage.service.StorageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import static com.asalavei.cloudfilestorage.common.Constants.REDIRECT_HOME;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/files")
+public class FileController {
+
+    private final StorageService storageService;
+
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file")MultipartFile file, @AuthenticationPrincipal UserPrincipal userPrincipal) {
+        storageService.uploadFile(userPrincipal.getId(),file);
+        return REDIRECT_HOME;
+    }
+}
