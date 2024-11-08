ac4d055595f034af56f849fcb94b4b70452a9c78
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-08 21:01:27 +0400

    feat: implement list items in `MinioStorageService`

--- src/main/java/com/asalavei/cloudfilestorage/service/MinioStorageService.java
+++ src/main/java/com/asalavei/cloudfilestorage/service/MinioStorageService.java
@@ -1,7 +1,11 @@
 package com.asalavei.cloudfilestorage.service;
 
+import com.asalavei.cloudfilestorage.dto.ItemDto;
+import io.minio.ListObjectsArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
+import io.minio.Result;
+import io.minio.messages.Item;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -9,12 +13,15 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MinioStorageService implements StorageService {
 
-    private static final String OBJECT_NAME = "user-%s-files/%s";
+    private static final String PREFIX = "user-%s-files/";
+    private static final String OBJECT_NAME = PREFIX + "%s";
 
     @Value("${minio.bucket.name}")
     private String bucketName;
@@ -53,5 +60,34 @@ public class MinioStorageService implements StorageService {
         } catch (Exception e) {
             throw new RuntimeException("Failed to create folder: " + folderName, e);
         }
+    }
+
+    public List<ItemDto> listItems(Long userId, String folderName) {
+        String prefix = String.format(PREFIX, userId);
+        String folderPath = String.format(OBJECT_NAME, userId, folderName);
+        List<ItemDto> items = new ArrayList<>();
+
+        try {
+            Iterable<Result<Item>> results = minioClient.listObjects(
+                    ListObjectsArgs.builder()
+                            .bucket(bucketName)
+                            .prefix(folderPath)
+                            .recursive(false)
+                            .build()
+            );
+
+            for (Result<Item> result : results) {
+                Item item = result.get();
+                String[] parts = item.objectName().split("/");
+                items.add(ItemDto.builder()
+                        .name(parts[parts.length - 1])
+                        .path(item.objectName().replace(prefix, ""))
+                        .build());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to view files in folder: " + folderPath, e);
+        }
+
+        return items;
     }
 }
--- src/main/java/com/asalavei/cloudfilestorage/service/StorageService.java
+++ src/main/java/com/asalavei/cloudfilestorage/service/StorageService.java
@@ -1,9 +1,14 @@
 package com.asalavei.cloudfilestorage.service;
 
+import com.asalavei.cloudfilestorage.dto.ItemDto;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface StorageService {
-    void uploadFile(Long userId, MultipartFile file);
+    void addFile(Long userId, MultipartFile file);
 
     void createFolder(Long userId, String folderName);
+
+    List<ItemDto> listItems(Long userId, String folderPath);
 }
