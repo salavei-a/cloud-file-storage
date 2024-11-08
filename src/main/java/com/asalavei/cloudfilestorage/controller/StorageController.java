69b885d3a9c599f3865ddef8918ccef5d6c59ac2
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-08 23:59:52 +0400

    refactor: move search to `StorageController`

--- src/main/java/com/asalavei/cloudfilestorage/controller/StorageController.java
+++ src/main/java/com/asalavei/cloudfilestorage/controller/StorageController.java
@@ -1,10 +1,13 @@
 package com.asalavei.cloudfilestorage.controller;
 
+import com.asalavei.cloudfilestorage.dto.ItemDto;
 import com.asalavei.cloudfilestorage.security.UserPrincipal;
 import com.asalavei.cloudfilestorage.service.StorageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 import static com.asalavei.cloudfilestorage.common.Constants.REDIRECT_HOME;
+import static com.asalavei.cloudfilestorage.common.Constants.SEARCH_VIEW;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,6 +25,21 @@ import static com.asalavei.cloudfilestorage.common.Constants.REDIRECT_HOME;
 public class StorageController {
 
     private final StorageService storageService;
+
+    @GetMapping("/search")
+    public String searchItems(Model model) {
+        List<ItemDto> items = List.of(
+                new ItemDto("3dd.png", "/3dd.png"),
+                new ItemDto("New Text Document.txt", "/New Text Document.txt"),
+                new ItemDto("Screenshot 2024-11-07 213438.png", "/test/Screenshot 2024-11-07 213438.png"),
+                new ItemDto("ff/", "/test/ff/"),
+                new ItemDto("1 (3).png", "/test/ff/efe/1 (3).png")
+        );
+
+        model.addAttribute("items", items);
+
+        return SEARCH_VIEW;
+    }
 
     @PostMapping
     public String upload(@RequestParam("files") List<MultipartFile> files, @AuthenticationPrincipal UserPrincipal userPrincipal) {
