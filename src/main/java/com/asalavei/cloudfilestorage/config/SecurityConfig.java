204c057a638fc814033f6c5494df4db8e6a7b8b1
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-08 13:41:17 +0400

    chore: disable CSRF in Spring Security

--- src/main/java/com/asalavei/cloudfilestorage/config/SecurityConfig.java
+++ src/main/java/com/asalavei/cloudfilestorage/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,7 +22,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/auth/**", "/favicon.ico")
                         .permitAll()
                         .anyRequest()
