773ee5440f35e419fea41eea7541a9f71f3b656e
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-08 21:13:11 +0400

    feat: add breadcrumbs and item list items to home page

--- src/main/java/com/asalavei/cloudfilestorage/controller/HomeController.java
+++ src/main/java/com/asalavei/cloudfilestorage/controller/HomeController.java
@@ -1,9 +1,15 @@
 package com.asalavei.cloudfilestorage.controller;
 
+import com.asalavei.cloudfilestorage.security.UserPrincipal;
+import com.asalavei.cloudfilestorage.service.StorageService;
+import com.asalavei.cloudfilestorage.util.BreadcrumbsUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import static com.asalavei.cloudfilestorage.common.Constants.HOME_VIEW;
 
@@ -12,8 +18,13 @@ import static com.asalavei.cloudfilestorage.common.Constants.HOME_VIEW;
 @RequestMapping("/")
 public class HomeController {
 
+    private final StorageService storageService;
+
     @GetMapping
-    public String homePage() {
+    public String homePage(@RequestParam(value = "path", required = false, defaultValue = "") String path, Model model,
+                           @AuthenticationPrincipal UserPrincipal userPrincipal) {
+        model.addAttribute("breadcrumbs", BreadcrumbsUtil.buildBreadcrumbs(path));
+        model.addAttribute("items", storageService.listItems(userPrincipal.getId(), path));
         return HOME_VIEW;
     }
 }
