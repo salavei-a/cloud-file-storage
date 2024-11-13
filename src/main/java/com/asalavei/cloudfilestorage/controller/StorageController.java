64ab9f9996bbee625ee2c58803f390e421f52401
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-13 17:49:17 +0400

    feat: add delete endpoint to `StorageController`

--- src/main/java/com/asalavei/cloudfilestorage/controller/StorageController.java
+++ src/main/java/com/asalavei/cloudfilestorage/controller/StorageController.java
@@ -10,9 +10,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -82,6 +84,13 @@ public class StorageController {
                                @RequestParam(value = "path", defaultValue = "/") String path) {
         storageService.createFolder(userPrincipal.getId(), folderName, path);
         return "redirect:/?path=" + path;
+    }
+
+    @DeleteMapping("/{*path}")
+    public String delete(@PathVariable("path") String path, @AuthenticationPrincipal UserPrincipal userPrincipal,
+                         @RequestHeader(value = "Referer", defaultValue = "/") String referer) {
+        storageService.delete(userPrincipal.getId(), path);
+        return "redirect:" + referer;
     }
 
     private static String generateZipFilename(String path) {
