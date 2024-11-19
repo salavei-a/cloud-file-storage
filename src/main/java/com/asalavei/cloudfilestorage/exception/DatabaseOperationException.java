2aead005004f68ad7d8a5c647218962e8e02c97e
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-19 22:36:26 +0400

    feat: add `AppRuntimeException` and `DatabaseOperationException`

---
+++ src/main/java/com/asalavei/cloudfilestorage/exception/DatabaseOperationException.java
@@ -0,0 +1,8 @@
+package com.asalavei.cloudfilestorage.exception;
+
+public class DatabaseOperationException extends AppRuntimeException {
+
+    public DatabaseOperationException(String message) {
+        super(message);
+    }
+}
