7c1a11a158dc2d0f6311bae60388212b89377884
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-19 22:57:49 +0400

    feat: add `UserAlreadyExistsException`

---
+++ src/main/java/com/asalavei/cloudfilestorage/exception/UserAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.asalavei.cloudfilestorage.exception;
+
+public class UserAlreadyExistsException extends AppRuntimeException {
+
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
