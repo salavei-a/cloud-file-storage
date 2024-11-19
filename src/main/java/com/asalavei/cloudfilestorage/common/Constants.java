8b5104971ebbf7e6722d9f46b0cee340c7f24d01
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-19 22:39:58 +0400

    feat: add constants for error views (404 and 505)

--- src/main/java/com/asalavei/cloudfilestorage/common/Constants.java
+++ src/main/java/com/asalavei/cloudfilestorage/common/Constants.java
@@ -20,4 +20,6 @@ public class Constants {
     public static final String SIGNUP_VIEW = "auth/signup";
     public static final String HOME_VIEW = "home";
     public static final String SEARCH_VIEW = "search";
+    public static final String ERROR_404_VIEW = "error/404";
+    public static final String ERROR_500_VIEW = "error/500";
 }
