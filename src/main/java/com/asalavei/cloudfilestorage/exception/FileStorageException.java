ba6909ca9de53879f0c02ad3eb815c0d3f364bca
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-20 23:05:27 +0400

    feat: add `FileStorageException`

---
+++ src/main/java/com/asalavei/cloudfilestorage/exception/FileStorageException.java
@@ -0,0 +1,8 @@
+package com.asalavei.cloudfilestorage.exception;
+
+public class FileStorageException extends AppRuntimeException {
+
+    public FileStorageException(String message) {
+        super(message);
+    }
+}
