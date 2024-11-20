830d3c3793ce69ef0825ca24ebc3aaa665d4bae9
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-20 21:50:43 +0400

    feat: add `MinioOperationException`

---
+++ src/main/java/com/asalavei/cloudfilestorage/exception/MinioOperationException.java
@@ -0,0 +1,8 @@
+package com.asalavei.cloudfilestorage.exception;
+
+public class MinioOperationException extends AppRuntimeException{
+
+    public MinioOperationException(String message) {
+        super(message);
+    }
+}
