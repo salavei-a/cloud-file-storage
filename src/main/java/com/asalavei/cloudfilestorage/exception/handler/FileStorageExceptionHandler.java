9c521914bfef89a8ff24b67ae4236b78bf2872c0
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-21 17:42:08 +0400

    feat: display error message on frontend for exceeded file size limit

--- src/main/java/com/asalavei/cloudfilestorage/exception/handler/FileStorageExceptionHandler.java
+++ src/main/java/com/asalavei/cloudfilestorage/exception/handler/FileStorageExceptionHandler.java
@@ -4,11 +4,13 @@ import com.asalavei.cloudfilestorage.exception.FileStorageException;
 import com.asalavei.cloudfilestorage.exception.MinioOperationException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import static com.asalavei.cloudfilestorage.common.Constants.ERROR_500_VIEW;
@@ -18,6 +20,9 @@ import static com.asalavei.cloudfilestorage.common.Constants.HOME_URL;
 @ControllerAdvice
 @Order(1)
 public class FileStorageExceptionHandler {
+
+    @Value("${spring.servlet.multipart.max-file-size}")
+    private String maxFileSize;
 
     @ExceptionHandler(MinioOperationException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -30,13 +35,23 @@ public class FileStorageExceptionHandler {
     public String handleFileStorageException(FileStorageException e, RedirectAttributes redirectAttributes,
                                              HttpServletRequest request) {
         log.error("File storage operation failed: {}", e.getMessage(), e);
+        redirectAttributes.addFlashAttribute("message", e.getMessage());
+        return "redirect:" + getReferer(request);
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public String handleMaxUploadSizeExceededException(RedirectAttributes redirectAttributes, HttpServletRequest request) {
+        redirectAttributes.addFlashAttribute("message", "The uploaded file is too large. Maximum allowed size is " + maxFileSize);
+        return "redirect:" + getReferer(request);
+    }
+
+    private String getReferer(HttpServletRequest request) {
         String referer = request.getHeader("Referer");
 
         if (referer == null || referer.isBlank()) {
             referer = HOME_URL;
         }
 
-        redirectAttributes.addFlashAttribute("message", e.getMessage());
-        return "redirect:" + referer;
+        return referer;
     }
 }
