f7a8bc1f6804e1d46626bd10011ecc19b946b2c3
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-22 21:19:00 +0400

    feat: add `NoObjectFoundException`

--- src/main/java/com/asalavei/cloudfilestorage/exception/MinioOperationException.java
+++ src/main/java/com/asalavei/cloudfilestorage/exception/MinioOperationException.java
@@ -1,10 +1,6 @@
 package com.asalavei.cloudfilestorage.exception;
 
-public class MinioOperationException extends AppRuntimeException{
-
-    public MinioOperationException(String message) {
-        super(message);
-    }
+public class MinioOperationException extends AppRuntimeException {
 
     public MinioOperationException(String message, Throwable cause) {
         super(message, cause);
---
+++ src/main/java/com/asalavei/cloudfilestorage/exception/NoObjectFoundException.java
@@ -0,0 +1,8 @@
+package com.asalavei.cloudfilestorage.exception;
+
+public class NoObjectFoundException extends AppRuntimeException {
+
+    public NoObjectFoundException(String message) {
+        super(message);
+    }
+}
