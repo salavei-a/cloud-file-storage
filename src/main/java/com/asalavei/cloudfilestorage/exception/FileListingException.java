b5a90cfe9a382e51db724359892686a07f9a75c0
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-22 12:36:58 +0400

    feat: add `FileListingException`

---
+++ src/main/java/com/asalavei/cloudfilestorage/exception/FileListingException.java
@@ -0,0 +1,8 @@
+package com.asalavei.cloudfilestorage.exception;
+
+public class FileListingException extends AppRuntimeException {
+
+    public FileListingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
