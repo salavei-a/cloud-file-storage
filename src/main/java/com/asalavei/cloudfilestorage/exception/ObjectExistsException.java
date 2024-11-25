1ac8ffe06b754a0068ce9d9cf936ae4020df601d
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-25 18:43:05 +0400

    feat: add `ObjectExistsException`

---
+++ src/main/java/com/asalavei/cloudfilestorage/exception/ObjectExistsException.java
@@ -0,0 +1,8 @@
+package com.asalavei.cloudfilestorage.exception;
+
+public class ObjectExistsException extends AppRuntimeException {
+
+    public ObjectExistsException(String message) {
+        super(message);
+    }
+}
