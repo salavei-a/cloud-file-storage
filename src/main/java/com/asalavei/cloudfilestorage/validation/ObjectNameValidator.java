ba1b91dbf3f01a0e6c4fbb49145c831c4116b342
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-26 21:40:25 +0400

    feat: add `ValidObjectName` constraint and `ObjectNameValidator`

---
+++ src/main/java/com/asalavei/cloudfilestorage/validation/ObjectNameValidator.java
@@ -0,0 +1,21 @@
+package com.asalavei.cloudfilestorage.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.regex.Pattern;
+
+public class ObjectNameValidator implements ConstraintValidator<ValidObjectName, String> {
+
+    private static final String PATH_PATTERN = "^[^/]*$";
+    private static final Pattern PATTERN = Pattern.compile(PATH_PATTERN);
+
+    @Override
+    public boolean isValid(final String name, final ConstraintValidatorContext constraintValidatorContext) {
+        if (".".equals(name) || "..".equals(name)) {
+            return false;
+        }
+
+        return PATTERN.matcher(name).matches();
+    }
+}
---
+++ src/main/java/com/asalavei/cloudfilestorage/validation/ValidObjectName.java
@@ -0,0 +1,21 @@
+package com.asalavei.cloudfilestorage.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({FIELD})
+@Retention(RUNTIME)
+@Constraint(validatedBy = ObjectNameValidator.class)
+public @interface ValidObjectName {
+    String message() default "Name cannot contain '/' and cannot be '.' or '..'";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
