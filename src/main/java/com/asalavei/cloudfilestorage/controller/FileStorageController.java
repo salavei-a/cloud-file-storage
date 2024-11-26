2eb7fdfd2fa45d4ee105887649307e9da38a0a05
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-11-26 21:47:08 +0400

    feat: add validation for object name and path in controllers

--- src/main/java/com/asalavei/cloudfilestorage/controller/FileStorageController.java
+++ src/main/java/com/asalavei/cloudfilestorage/controller/FileStorageController.java
@@ -1,9 +1,12 @@
 package com.asalavei.cloudfilestorage.controller;
 
+import com.asalavei.cloudfilestorage.dto.ObjectRequestDto;
 import com.asalavei.cloudfilestorage.security.UserPrincipal;
 import com.asalavei.cloudfilestorage.service.FileStorageService;
 import com.asalavei.cloudfilestorage.util.HttpUtils;
+import com.asalavei.cloudfilestorage.validation.ValidObjectPath;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
@@ -11,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -25,17 +29,19 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
+import static com.asalavei.cloudfilestorage.common.Constants.MESSAGE_ATTRIBUTE;
 import static com.asalavei.cloudfilestorage.common.Constants.SEARCH_VIEW;
 
 @Controller
 @RequiredArgsConstructor
+@Validated
 @RequestMapping("/storage")
 public class FileStorageController {
 
     private final FileStorageService fileStorageService;
 
     @GetMapping("/download")
-    public ResponseEntity<InputStreamResource> downloadFile(@RequestParam("path") String path,
+    public ResponseEntity<InputStreamResource> downloadFile(@RequestParam("path") @ValidObjectPath String path,
                                                             @AuthenticationPrincipal UserPrincipal userPrincipal) {
         InputStream inputStream = fileStorageService.downloadFile(userPrincipal.getId(), path);
         String fileName = UriUtils.encode(fileStorageService.getFileName(path), StandardCharsets.UTF_8);
@@ -48,7 +54,7 @@ public class FileStorageController {
     }
 
     @GetMapping("/download-multiple")
-    public ResponseEntity<InputStreamResource> downloadFolder(@RequestParam("path") String path,
+    public ResponseEntity<InputStreamResource> downloadFolder(@RequestParam("path") @ValidObjectPath String path,
                                                               @AuthenticationPrincipal UserPrincipal userPrincipal) {
         InputStream inputStream = fileStorageService.downloadFolderAsZip(userPrincipal.getId(), path);
         String fileName = UriUtils.encode(fileStorageService.generateZipFilename(path), StandardCharsets.UTF_8);
@@ -66,40 +72,39 @@ public class FileStorageController {
     }
 
     @PostMapping("/upload")
-    public String upload(@RequestParam("files") List<MultipartFile> files, @AuthenticationPrincipal UserPrincipal userPrincipal,
-                         @RequestParam(value = "path", defaultValue = "/") String path, RedirectAttributes redirectAttributes,
-                         HttpServletRequest request) {
+    public String upload(@RequestParam(value = "path", defaultValue = "/") @ValidObjectPath String path,
+                         @RequestParam("files") List<MultipartFile> files, @AuthenticationPrincipal UserPrincipal userPrincipal,
+                         RedirectAttributes redirectAttributes, HttpServletRequest request) {
         files.forEach(file -> fileStorageService.upload(userPrincipal.getId(), file, path));
 
-        redirectAttributes.addFlashAttribute("message", "Upload complete");
+        redirectAttributes.addFlashAttribute(MESSAGE_ATTRIBUTE, "Upload complete");
         return HttpUtils.redirectToReferer(request);
     }
 
     @PostMapping("/folders")
-    public String createFolder(@RequestParam("folderName") String folderName, @AuthenticationPrincipal UserPrincipal userPrincipal,
-                               @RequestParam(value = "path", defaultValue = "/") String path, RedirectAttributes redirectAttributes,
-                               HttpServletRequest request) {
-        fileStorageService.createFolder(userPrincipal.getId(), folderName, path);
+    public String createFolder(@Valid ObjectRequestDto objectRequestDto, @AuthenticationPrincipal UserPrincipal userPrincipal,
+                               RedirectAttributes redirectAttributes, HttpServletRequest request) {
+        fileStorageService.createFolder(userPrincipal.getId(), objectRequestDto.getName(), objectRequestDto.getPath());
 
-        redirectAttributes.addFlashAttribute("message", "Folder created successfully");
+        redirectAttributes.addFlashAttribute(MESSAGE_ATTRIBUTE, "Folder created successfully");
         return HttpUtils.redirectToReferer(request);
     }
 
     @PatchMapping
-    public String rename(@RequestParam("newName") String newName, @AuthenticationPrincipal UserPrincipal userPrincipal,
-                         @RequestParam("path") String path, RedirectAttributes redirectAttributes, HttpServletRequest request) {
-        fileStorageService.rename(userPrincipal.getId(), newName, path);
+    public String rename(@Valid ObjectRequestDto objectRequestDto, @AuthenticationPrincipal UserPrincipal userPrincipal,
+                         RedirectAttributes redirectAttributes, HttpServletRequest request) {
+        fileStorageService.rename(userPrincipal.getId(), objectRequestDto.getName(), objectRequestDto.getPath());
 
-        redirectAttributes.addFlashAttribute("message", "Renamed successfully");
+        redirectAttributes.addFlashAttribute(MESSAGE_ATTRIBUTE, "Renamed successfully");
         return HttpUtils.redirectToReferer(request);
     }
 
     @DeleteMapping
-    public String delete(@RequestParam("path") String path, @AuthenticationPrincipal UserPrincipal userPrincipal,
+    public String delete(@RequestParam("path") @ValidObjectPath String path, @AuthenticationPrincipal UserPrincipal userPrincipal,
                          RedirectAttributes redirectAttributes, HttpServletRequest request) {
         fileStorageService.delete(userPrincipal.getId(), path);
 
-        redirectAttributes.addFlashAttribute("message", "Deleted successfully");
+        redirectAttributes.addFlashAttribute(MESSAGE_ATTRIBUTE, "Deleted successfully");
         return HttpUtils.redirectToReferer(request);
     }
 }
--- src/main/java/com/asalavei/cloudfilestorage/controller/HomeController.java
+++ src/main/java/com/asalavei/cloudfilestorage/controller/HomeController.java
@@ -3,10 +3,12 @@ package com.asalavei.cloudfilestorage.controller;
 import com.asalavei.cloudfilestorage.security.UserPrincipal;
 import com.asalavei.cloudfilestorage.service.FileStorageService;
 import com.asalavei.cloudfilestorage.util.BreadcrumbsUtil;
+import com.asalavei.cloudfilestorage.validation.ValidObjectPath;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,13 +17,14 @@ import static com.asalavei.cloudfilestorage.common.Constants.HOME_VIEW;
 
 @Controller
 @RequiredArgsConstructor
+@Validated
 @RequestMapping("/")
 public class HomeController {
 
     private final FileStorageService fileStorageService;
 
     @GetMapping
-    public String homePage(@RequestParam(value = "path", defaultValue = "/") String path, Model model,
+    public String homePage(@RequestParam(value = "path", defaultValue = "/") @ValidObjectPath String path, Model model,
                            @AuthenticationPrincipal UserPrincipal userPrincipal) {
         model.addAttribute("breadcrumbs", BreadcrumbsUtil.buildBreadcrumbs(path));
         model.addAttribute("objects", fileStorageService.list(userPrincipal.getId(), path));
