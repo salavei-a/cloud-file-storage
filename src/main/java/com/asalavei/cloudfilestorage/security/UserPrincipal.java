52e016014c92128dd98ccccacd26e5d7b82f38cd
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-12-04 22:38:14 +0400

    refactor: remove redundant methods in `UserPrincipal`

--- src/main/java/com/asalavei/cloudfilestorage/security/UserPrincipal.java
+++ src/main/java/com/asalavei/cloudfilestorage/security/UserPrincipal.java
@@ -31,24 +31,4 @@ public class UserPrincipal implements UserDetails {
     public Long getId() {
         return user.getId();
     }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
-    }
 }
