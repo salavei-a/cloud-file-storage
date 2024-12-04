9aee722fcb37b37d934865970686f84b718d0dc0
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-12-04 22:53:47 +0400

    refactor: remove `WebConfig` and move bean to `CloudFileStorageApplication`

--- src/main/java/com/asalavei/cloudfilestorage/CloudFileStorageApplication.java
+++ src/main/java/com/asalavei/cloudfilestorage/CloudFileStorageApplication.java
@@ -2,6 +2,8 @@ package com.asalavei.cloudfilestorage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class CloudFileStorageApplication {
@@ -10,4 +12,8 @@ public class CloudFileStorageApplication {
         SpringApplication.run(CloudFileStorageApplication.class, args);
     }
 
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
 }
